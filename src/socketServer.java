ee8de27c316cd1766bbf5953670ece323c8c837f
Andrew <avashchenock@gmail.com>
2019-01-02 19:31:20 +0200

    getting messages and last == "null" from 1 client

--- src/socketServer.java
+++ src/socketServer.java
@@ -6,34 +6,26 @@ import java.io.InputStreamReader;
 
 public class socketServer {
     public void run() throws IOException {
-        System.out.println("Server running!");
-        ServerSocket listener = new ServerSocket(9090);
-        try {
+        try (ServerSocket listener = new ServerSocket(9090)) {
+            System.out.println("Server running!");
             while (true) {
-                Socket socket = listener.accept();
-                try {
+                try (Socket socket = listener.accept()) {
                     BufferedReader input =
                             new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    if (input.readLine() != null) {
+                    //if (input.readLine() != null) {
                         System.out.println("\n\nMessage received!\n On port: " + socket.getPort()
                                 + "\nFrom address: "
                                 + socket.getInetAddress()
                                 + "\nMessage: "
                                 + input.readLine()
                         );
-                        if (input.readLine() == "@exit")
-                            break;
-                    }
+                        //if (input.readLine() == "@exit")
+                            //break;
+                   // }
                     input.close();
                 }
-                finally{
-                    socket.close();
-                }
             }
+
         }
-        finally {
-            listener.close();
-        }
-        System.out.println("Listener closed");
     }
 }
